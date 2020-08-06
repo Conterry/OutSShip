b84b68b2460a44903fff6e2265d8824b851a6dbe
Conterry <59209260+Conterry@users.noreply.github.com>
2020-08-06 15:06:27 +0300

    ...

---
+++ Assets/MouveLandshavt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouveLandshavt : MonoBehaviour
+{
+
+    public GameObject SeaSpawn;
+    public Transform PointToSeaSpawn;
+    public GameObject Sea;
+
+    void Update()
+    {
+        Sea.transform.Translate(Vector3.back * Time.deltaTime);
+    }
+    
+    private void OnCollisionEnter(Collision Sea)
+    {
+        SpawnSea();
+    }
+
+    public void SpawnSea()
+    {
+        Instantiate(Sea, SeaSpawn.transform.position , Quaternion.identity);
+    }
+}
